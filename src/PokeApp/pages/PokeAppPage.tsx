4526a97040e424fe17d92f0212de96d6c4960421
gopichardo <gopichardoces@gmail.com>
2024-09-26 18:15:37 -0600

    PokeAppPage styling

--- src/PokeApp/pages/PokeAppPage.tsx
+++ src/PokeApp/pages/PokeAppPage.tsx
@@ -1,16 +1,24 @@
-import { Button } from "../../components/Buttons/StartButton"
-
+import { Box, Button, Grid2, Stack } from "@mui/material";
+import pokemonImgUrl from '../../assets/images/pokemon-logo.svg'
+import pokeballImgUrl from '../../assets/images/pokeball.png'
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import './PokeAppPage.css';
 
 export const PokeAppPage = () => {
 
-    const handleClickStart = () => {
-        alert('Start');
-    }
-
     return (
-        <>
-            Pokemon
-            <Button text="Start" handleClick={handleClickStart} />
-        </>
+        <Grid2 container>
+            <Grid2 size={{ xs: 12, md: 8, lg: 4, xl: 4 }}>
+                <Stack spacing={2} direction="column" display="flex" justifyContent="center" alignItems="center" height="100vh">
+                    <Box display="flex" justifyContent="center" flexDirection="column" alignItems="center">
+                        <img src={pokemonImgUrl} alt="Pokemon Logo" className="pokemon-logo" />
+                        <img src={pokeballImgUrl} alt="Pokeball Logo" className="pokeball" />
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Button href="/auth/login" color="primary" variant="contained" fullWidth={false} size="large" startIcon={<PlayCircleIcon />}>Start</Button>
+                    </Box>
+                </Stack>
+            </Grid2>
+        </Grid2 >
     )
 }
